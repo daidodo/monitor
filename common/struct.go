f97586b18dc3a726eb4d07b84fe1d8e30279c17f
daidodo <daidodo@gmail.com>
2017-01-25 17:49:14 +0800

    change attr to uint32

--- common/struct.go
+++ common/struct.go
@@ -3,18 +3,19 @@ package common
 import "sync/atomic"
 
 type Node struct {
-	Attr  uint64
+	Attr  uint32
+	pad   uint32
 	Value uint64
 }
 
 type Mem []Node
 
-func (m Mem) FindNode(attr uint64) *Node {
+func (m Mem) FindNode(attr uint32) *Node {
 	if attr > 0 {
 		for i := 0; i < len(m); i++ {
 			n := &m[i]
 			if n.Attr == 0 {
-				if atomic.CompareAndSwapUint64(&n.Attr, 0, attr) {
+				if atomic.CompareAndSwapUint32(&n.Attr, 0, attr) {
 					return n
 				}
 			} else if attr == n.Attr {
