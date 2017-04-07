0647d773a78a5c9b5187e6822eb19f00e1f87b4e
daidodo <daidodo@gmail.com>
2017-04-07 14:20:26 +0800

    Minor mod;

--- attr/attr.go
+++ attr/attr.go
@@ -7,15 +7,6 @@ import (
 	"github.com/daidodo/overlord/inner"
 )
 
-var ns inner.Nodes
-
-func init() {
-	var err error
-	if ns, err = inner.Attach(false); err != nil {
-		panic(fmt.Errorf("Cannot init overlord/attr: %v", err))
-	}
-}
-
 // Add increments value for 'attr' by 'delta'
 func Add(attr uint32, delta uint64) {
 	if delta > 0 {
@@ -31,5 +22,14 @@ func Set(attr uint32, value uint64) {
 	n := ns.FindNode(attr)
 	if n != nil && attr == n.Attr {
 		atomic.StoreUint64(&n.Value, value)
+	}
+}
+
+var ns inner.Nodes
+
+func init() {
+	var err error
+	if ns, err = inner.Attach(false); err != nil {
+		panic(fmt.Errorf("Cannot init overlord/attr: %v", err))
 	}
 }
