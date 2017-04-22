aacf014021b1dabdbace649a8d0ec0176c9572fe
daidodo <daidodo@gmail.com>
2017-04-23 00:29:23 +0800

    Mod db size tool;

--- tools/db_size.sh
+++ tools/db_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mysql -h db -u root -pmysql -e "select table_name, table_rows, round((data_length+index_length)/1024,2) as 'size(KB)' from information_schema.tables where information_schema.tables.table_schema='overlord'"
+mysql -h db -u root -pmysql -e "select table_name, table_rows, data_length, index_length, round((data_length+index_length)/1024,2) as 'size(KB)' from information_schema.tables where information_schema.tables.table_schema='overlord'"
